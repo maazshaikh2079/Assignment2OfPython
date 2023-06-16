1b6ef4a8f230974ab6089b70583efc4a926a9f70
maazshaikh2079 <124486797+maazshaikh2079@users.noreply.github.com>
2023-06-16 12:32:41 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,218 @@
+# Info:-
+# Name   : Maaz 
+# Dept   : CO-B
+# Batch  : 4
+# Sub    : Python Assignment-2
+# Topic  : WAP to demonstrate the use of all types of operators.
+# Date   : 15-06-2023
+
+# Program:-
+# Initialization:-
+print("Initialization:-");
+n1=10;
+n2=2;
+print("[First number]  := n1 =",n1);
+print("[Second number] := n2 =",n2);
+print();
+
+# Arithmetic operators:-
+print("Arithmetic operators:-");
+# +
+print("Addition       :",n1,'+',n2,' =',(n1+n2));
+# -
+print("Subtraction    :",n1,'-',n2,' =',(n1-n2));
+# *
+print("Multiplication :",n1,'*',n2,' =',(n1*n2));
+# /
+print("Division       :",n1,'/',n2,' =',(n1/n2));
+# %
+print("Modulas        :",n1,'%',n2,' =',(n1%n2));
+# //
+print("Floor Division :",n1,'//',n2,'=',(n1//n2));
+# **
+print("Exponentation  :",n1,'**',n2,'=',(n1**n2));
+print();
+
+# Assignment operators:-
+print("Assignment operators:-");
+# +=
+n1=10;
+print("n1 =",n1);
+n1+=1;
+print("n1 += 1  := n1 =",n1);
+# -=
+n1=10;
+print("n1 =",n1);
+n1-=1;
+print("n1 -= 1  := n1 =",n1);
+# *=
+n1=10;
+print("n1 =",n1);
+n1*=1;
+print("n1 *= 1  := n1 =",n1);
+# /=
+n1=10;
+print("n1 =",n1);
+n1/=1;
+print("n1 /= 1  := n1 =",n1);
+# %=
+n1=10;
+print("n1 =",n1);
+n1%=1;
+print("n1 %= 1  := n1 =",n1);
+# //=
+n1=10;
+print("n1 =",n1);
+n1//=1;
+print("n1 //= 1 := n1 =",n1);
+# **=
+n1=10;
+print("n1 =",n1);
+n1**=1;
+print("n1 **= 1 := n1 =",n1);
+print();
+
+# comparision operators:-
+print("comparision operators:-");
+print("n1 =",n1);
+print("n2 =",n2);
+print();
+# >=
+print("n1 >= n2 :=",n1,">=",n2,":=",(n1>=n2));
+# <=
+print("n1 <= n2 :=",n1,"<=",n2,":=",(n1>=n2));
+# ==
+print("n1 == n2 :=",n1,"==",n2,":=",(n1>=n2));
+# !=
+print("n1 != n2 :=",n1,"!=",n2,":=",(n1!=n2));
+# <
+print("n1 < n2  :=",n1,"<",n2," :=",(n1<n2));
+# >
+print("n1 > n2  :=",n1,">",n2," :=",(n1>n2));
+print();
+
+# Initialization:-
+print("Initialization:-")
+b1=True;
+b2=False;
+print("b1 =",b1);
+print("b2 =",b2);
+print();
+
+# Logical operators:-
+print("Logical operators:-");
+# logical and
+print("b1 and b2 :=",b1 and b2);
+# logical or
+print("b1 or b2  :=",b1 or b2);
+# logical not
+print("not b1    :=",not b1);
+print();
+
+# Bitwise operators:-
+print("Bitwise operators:-");
+# bitwise and : &
+print("b1 & b2  :=",b1,'&',b2," :=",(b1 & b2));
+# bitwise or  : |
+print("b1 | b2  :=",b1,'|',b2," :=",(b1 | b2));
+# bitwise xor : ^
+print("b1 ^ b2  :=",b1,'^',b2," :=",(b1 ^ b2));
+# bitwise right sift : >>
+print("b1 >> b2 :=",b1,'>>',b2,":=",(b1 >> b2));
+# bitwise left sift  : <<
+print("b1 << b2 :=",b1,'<<',b2,":=",(b1 << b2));
+print();
+
+# Identity operators:-
+print("Identity operators:-");
+# is
+print("b1 is b2     :=",b1,"is",b2,"    :=",(b1 is b2));
+# is not
+print("b1 is not b2 :=",b1,"is not",b2,":=",(b1 is not b2));
+print();
+
+# Initialization:-
+print("Initialization:-");
+name="Maaz";
+print("name =",name);
+print();
+
+# Membership operators:-
+print("Membership operators:-");
+# in 
+print("'M' in name     :=",'M' in name);
+print("'q' in name     :=",'q' in name);
+# not in
+print("'M' not in name :=",'M' not in name);
+print("'q' not in name :=",'q' not in name);
+print();
+
+# Output:-
+# Initialization:-
+# [First number]  := n1 = 10
+# [Second number] := n2 = 2
+
+# Arithmetic operators:-
+# Addition       : 10 + 2  = 12
+# Subtraction    : 10 - 2  = 8
+# Multiplication : 10 * 2  = 20
+# Division       : 10 / 2  = 5.0
+# Modulas        : 10 % 2  = 0
+# Floor Division : 10 // 2 = 5
+# Exponentation  : 10 ** 2 = 100
+
+# Assignment operators:-
+# n1 = 10
+# n1 += 1  := n1 = 11
+# n1 = 10
+# n1 -= 1  := n1 = 9
+# n1 = 10
+# n1 *= 1  := n1 = 10
+# n1 = 10
+# n1 /= 1  := n1 = 10.0
+# n1 = 10
+# n1 %= 1  := n1 = 0
+# n1 = 10
+# n1 //= 1 := n1 = 10
+# n1 = 10
+# n1 **= 1 := n1 = 10
+
+# comparision operators:-
+# n1 = 10
+# n2 = 2
+
+# n1 >= n2 := 10 >= 2 := True
+# n1 <= n2 := 10 <= 2 := True
+# n1 == n2 := 10 == 2 := True
+# n1 != n2 := 10 != 2 := True
+# n1 < n2  := 10 < 2  := False
+# n1 > n2  := 10 > 2  := True
+
+# Initialization:-
+# b1 = True
+# b2 = False
+
+# Logical operators:-
+# b1 and b2 := False
+# b1 or b2  := True
+# not b1    := False
+
+# Bitwise operators:-
+# b1 & b2  := True & False  := False
+# b1 | b2  := True | False  := True
+# b1 ^ b2  := True ^ False  := True
+# b1 >> b2 := True >> False := 1
+# b1 << b2 := True << False := 1
+
+# Identity operators:-
+# b1 is b2     := True is False     := False
+# b1 is not b2 := True is not False := True
+
+# Initialization:-
+# name = Maaz
+
+# Membership operators:-
+# 'M' in name     := True
+# 'q' in name     := False
+# 'M' not in name := False
+# 'q' not in name := True
